7c9e5eb3108da2d7b2e796834cd6ee3a0eb4ef2e
stevenanthonyrevo <stevenanthonyrevo@live.com>
2024-03-09 19:48:41 -0500

    Add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+function MyButton() {
+  function handleClick() {
+    alert("You have clicked me!");
+  }
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  )
+}
+
 function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <MyButton></MyButton>
     </div>
   );
 }
