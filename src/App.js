cdeca1cda83f9bfdb02378dcbf2913669d4fd2b3
stevenanthonyrevo <stevenanthonyrevo@live.com>
2024-03-10 21:09:06 -0400

    Add Input with Button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
-import React from 'react';
+import React,  { useState } from 'react';
+
+function MyInput() {
+  const [content, setContent] = useState('');
+  function send(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const content = formData.get("content");
+    const button = event.target.button.value;
+
+    if (!content || content.trim() === "") {
+      window.alert("Content cannot be empty");
+      return;
+    }
+
+    alert(`formData: '${content}' submit was clicked input: '${button}'`);
+  }
+  return (
+    <div style={{ paddingBottom: '20px', paddingTop: '20px' }}>
+      <form onSubmit={send}>
+        <textarea 
+          name="content" 
+          rows={1} 
+          cols={40} 
+          placeholder='Describe yourself here...'
+          value={content} 
+          onChange={(event) => setContent(event.target.value)}  />
+        <br />
+        <button type="submit" name="button" value={content}>Click me</button>
+      </form>
+    </div>
+  )
+}
 
 function MyButton() {
   function handleClick() {
@@ -16,6 +48,7 @@ function App() {
     <div>
       <h1>Hello, React!</h1>
       <MyButton></MyButton>
+      <MyInput></MyInput>
     </div>
   );
 }
