770b474cd2e9a626faf45212ec0f3a115e475e1d
stevenanthonyrevo <stevenanthonyrevo@live.com>
2024-03-10 23:42:06 -0400

    Add Styled Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
 import React,  { useState } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  outline: none;
+  border-width: 0px;
+  border-radius: 3px;
+  box-sizing: border-box;
+  font-size: inherit;
+  font-weight: 500;
+  max-width: 100%;
+  text-align: center;
+  text-decoration: none;
+  transition: background 0.1s ease-out 0s, box-shadow 0.15s cubic-bezier(0.47, 0.03, 0.49, 1.38) 0s;
+  background: rgb(0, 82, 204);
+  cursor: pointer;
+  height: 32px;
+  line-height: 32px;
+  padding: 0px 12px;
+  vertical-align: middle;
+  width: auto;
+  font-size: 14px;
+  color: rgb(255, 255, 255);
+  &:hover {
+    background: rgb(0, 101, 255);
+    text-decoration: inherit;
+    transition-duration: 0s, 0.15s;
+    color: rgb(255, 255, 255);
+  }
+`;
 
 function MyInput() {
   const [content, setContent] = useState('');
@@ -26,7 +56,7 @@ function MyInput() {
           value={content} 
           onChange={(event) => setContent(event.target.value)}  />
         <br />
-        <button type="submit" name="button" value={content}>Click me</button>
+        <Button type="submit" name="button" value={content}>Click me</Button>
       </form>
     </div>
   )
@@ -37,9 +67,9 @@ function MyButton() {
     alert("You have clicked me!");
   }
   return (
-    <button onClick={handleClick}>
+    <Button onClick={handleClick}>
       Click me
-    </button>
+    </Button>
   )
 }
 
